46a4a118d76e5663f2099e6906ac94060483e5a5
Tiago Santos <tfsantosbr@gmail.com>
2019-12-02 02:28:39 -0300

    ok

---
+++ shared/Shared.Domain/ValueObjects/Address.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Domain.ValueObjects
+{
+    public class Address
+    {
+        public string Neighbourhood { get; set; }
+        public string Number { get; set; }
+    }
+}
